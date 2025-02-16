184f960712fda795d38c87242c8919ba3e03759c
miniaturerocks <info@miniaturerocks.com>
2025-02-16 12:48:16 +0900

    2025-02-16

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+//production development
+module.exports = {
+  mode: "production",
+  entry: {
+    app:path.resolve(__dirname, "src/App.jsx"),
+    },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name]-[fullhash].js",
+    //filename: "app.js",
+    publicPath:"/",
+  },
+  
+  resolve: {
+    modules: [path.resolve(__dirname, "node_modules")],
+    extensions: [".js", ".jsx"],
+    alias:{"react/jsx-dev-runtime":"react/jsx-dev-runtime.js","react/jsx-runtime":"react/jsx-runtime.js"},
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env","@babel/preset-react"],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.html"),
+    }),
+
+  ],
+};
